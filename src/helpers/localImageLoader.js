dc1c983bae2759b903a70f32725ab6deacbd12d5
victorAnumudu <anumuduchukwuebuka@gmail.com>
2025-08-16 14:11:08 +0100

    image display fix

--- src/helpers/localImageLoader.js
+++ src/helpers/localImageLoader.js
@@ -1,9 +1,18 @@
 // const localImgLoader = (location) => require(`../assets/${location}`);
+// const localImgLoader = (location) => {
+//     if(!location){
+//         return `/src/assets/logos/Abia_logo.png`
+//     }else{
+//         return `/src/assets/${location}`
+//     }
+// };
+// export default localImgLoader
+
 const localImgLoader = (location) => {
     if(!location){
-        return `/assets/logos/Abia_logo.png`
+        return `./assets/logos/Abia_logo.png`
     }else{
-        return `/assets/${location}`
+        return `./assets/${location}`
     }
 };
 export default localImgLoader
