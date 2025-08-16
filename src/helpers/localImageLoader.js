7e5bbdf4dc17823ada79fb187cf26f5a9fbdc002
victorAnumudu <anumuduchukwuebuka@gmail.com>
2025-08-16 13:49:18 +0100

    image display fix

--- src/helpers/localImageLoader.js
+++ src/helpers/localImageLoader.js
@@ -1,9 +1,9 @@
 // const localImgLoader = (location) => require(`../assets/${location}`);
 const localImgLoader = (location) => {
     if(!location){
-        return `/src/assets/logos/Abia_logo.png`
+        return `/assets/logos/Abia_logo.png`
     }else{
-        return `/src/assets/${location}`
+        return `/assets/${location}`
     }
 };
 export default localImgLoader
